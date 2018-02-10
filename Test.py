b1a49a5372f3418638f9a04654c6a0bbbf225625
Hsiao Ngo <t1ngo@ryerson.ca>
2018-02-10 14:30:46 -0500

    Fixed "IndentationError: expected an indented block (Test.py, line 10)"

--- Test.py
+++ Test.py
@@ -3,23 +3,23 @@ import math
 
 class Test(unittest.TestCase):
 
-    def setUp(self):
-        pass
+	def setUp(self):
+		pass
 	
 	def test_addition(self):
-        self.assertEqual(addition(11,2),13)
+		self.assertEqual(addition(11,2),13)
 
-    def test_subtraction(self):
-        self.assertEqual(subtraction(5,1),4)
+	def test_subtraction(self):
+		self.assertEqual(subtraction(5,1),4)
 
-    def test_multiplication(self):
-        self.assertEqual(multiplication(4,5),20)
+	def test_multiplication(self):
+		self.assertEqual(multiplication(4,5),20)
 	
 	def test_division(self):
-        self.assertEqual(division(12,2),6)
+		self.assertEqual(division(12,2),6)
 	
 	def test_divisionbyzero(self):
-        with self.assertRaises(ZeroDivisionError):division(7,0)
+		with self.assertRaises(ZeroDivisionError):division(7,0)
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
