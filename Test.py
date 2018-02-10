b5bc18e081f022c42cdc8d457182329ecf3a8d56
Hsiao Ngo <t1ngo@ryerson.ca>
2018-02-10 14:36:10 -0500

    Fixing error in test.py

--- Test.py
+++ Test.py
@@ -7,19 +7,19 @@ class Test(unittest.TestCase):
 		pass
 	
 	def test_addition(self):
-		self.assertEqual(addition(11,2),13)
+		self.assertEqual(math,addition(11,2),13)
 
 	def test_subtraction(self):
-		self.assertEqual(subtraction(5,1),4)
+		self.assertEqual(math,subtraction(5,1),4)
 
 	def test_multiplication(self):
-		self.assertEqual(multiplication(4,5),20)
+		self.assertEqual(math,multiplication(4,5),20)
 	
 	def test_division(self):
-		self.assertEqual(division(12,2),6)
+		self.assertEqual(math,division(12,2),6)
 	
 	def test_divisionbyzero(self):
-		with self.assertRaises(ZeroDivisionError):division(7,0)
+		with self.assertRaises(ZeroDivisionError):math,division(7,0)
 
 if __name__ == '__main__':
 	unittest.main()
--- math.py
+++ math.py
@@ -1,13 +1,13 @@
-def addition (x, y):
+def addition(x, y):
 	return x+y
 
-def subtrsction (x, y):
+def subtrsction(x, y):
 	return x-y
 
-def multiplication (x, y):
+def multiplication(x, y):
 	return x*y
 
-def division (x, y):
+def division(x, y):
 	if y==0:
 		raise ZeroDivisionError('The divisor cannot be zero')
 		return x / y
